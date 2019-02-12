6924b36cf75ae7f17b9f21b4a9fc22cff5d8243b
Joe Leveille <joe.leveille@valpo.edu>
2019-02-12 12:01:07 -0600

    Create initial cpp file

---
+++ classScheduler.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //initialize class list
+  //ask for require class inputs
+  //ask for wanted class inputs
+  //figure out possible schedules
+  //print out all possible schedules
+}
