0dcdbf830f6f7e08a8206a31497d959b62377b67
Joe Leveille <joe.leveille@valpo.edu>
2019-04-05 12:03:28 -0500

    Joe-started to plan scheduling logic

--- app.cpp
+++ app.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //A struct to hold information about a class
 struct ClassEntry {
-	//The subject and number. Ex: "ECE-211"
+	//The subject and number. Ex: "ECE-211"      //could be split up into dept & courseNum
 	string subjectNumber;
 	//The course's name
 	string title;
@@ -33,7 +33,7 @@ struct ClassEntry {
 	
 	//Returns a string representation of the class - W.I.P.
 	string toString() {
-		std::string str = std::to_string(numCredits);
+		string str = to_string(numCredits);
 		//Erases everything from 1 right of the first non-matching character ('0' in this case) to the end (npos)
 		//This will get rid of all the extra zeros. Side effect: if an integral number x is used, get "x.", not "x.0"
 		str = str.erase(str.find_last_not_of('0') + 1, string::npos);
@@ -48,16 +48,66 @@ struct ClassEntry {
 
 //Read in the data from the path and return a vector of ClassEntry's holding the data
 vector<ClassEntry> readData(string path);
+void cmdInput(vector<ClassEntry> classes){
+	string required[10];
+	int i, totalReq;
+	for(i = 0; i<10;i++){
+		cout<<"Please enter a course you are required to take(0 if no more required): ";
+		cin >> required[i];
+		if(required[i]=="0"){
+			i--;
+			break;
+		}
+	}
+	totalReq = i+1;
+	cout<<totalReq<< " classes entered" << endl;
+	
+	cout<< "You have chosen the following classes: "<<endl;
+	bool classExists;
+	for(int j = 0; j<totalReq;j++){ //loop through required classes
+		classExists = false;
+		for(int k = 0; k<classes.size();k++){ //loop through offered classes
+			//TODO:Check that class number aligns with a class
+			if(required[j]==classes[k].subjectNumber){
+				//print class name if yes
+				classExists=true;
+				break;
+			}
+		}
+		if(classExists){
+			cout<< required[j] << endl;
+		}
+		else{
+			//print error if number doesn't exist
+			cout<<"A class that isn't offered"<<endl;
+		}
+	}
+}
 
 int main() {
 	vector<ClassEntry> classes = readData("schedule.csv");
-	cout << "Printing the data for each class" << '\n';
+	cout << "Printing the data for each class" << endl;
 	
 	for(int i = 0; i < classes.size(); i++) {
-		cout << classes[i].toString() << '\n' << '\n';
+		cout << classes[i].toString() << endl;
 	}
+	//
+	cmdInput(classes);
+
+	vector< vector <int> > reqSections; //This 2D vector will hold the times for each section(row) of each class(column)
+	for(vector<int> row in reqSections){
+		//find one pair of times in row one that doesn't conflict with one pair in row 2 & 3
+		for(int startTimeIndex = 0; startTimeIndex <totRows; startTimeIndex += 2){
+			if(row(startTimeIndex)>/*Next row start time*/ && ){
+				//
+			}
+		}
+	}
+	
 	return 0;
 }
+
+
 
 //Convert a time to an int. Time passed in should be of the form "12:30 AM", "2:45 PM" etc.
 int timeToInt(string str) {
